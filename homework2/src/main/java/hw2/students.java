5f3d061a9f4ebd87c207a60479f80c4788ae0c54
EduardTsvetkov <edvardas@inbox.ru>
2022-11-29 22:53:43 +0300

    Про оценки готово

---
+++ homework2/src/main/java/hw2/students.java
@@ -0,0 +1,42 @@
+package hw2;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+/**
+ * Чтение файла grades.json и формирование сообщения с оценками
+ *
+ */
+public class students {
+    public static void main(String[] args) {
+
+        JSONParser parser = new JSONParser();
+
+        try {     
+            
+            JSONArray jArr = (JSONArray) parser.parse(new FileReader("grades.json"));
+
+            for (Object o : jArr)
+            {
+                JSONObject person = (JSONObject) o;
+                String surname = (String) person.get("фамилия");
+                String grade = (String) person.get("оценка");
+                String discipline = (String) person.get("предмет");
+                System.out.printf("Студент %s получил %s по предмету %s.\n", surname, grade, discipline); 
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
